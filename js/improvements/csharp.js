d1429344941e0f89df8678a1ec0029902cce8ed1
Computer Virus <86645415+computer-virus@users.noreply.github.com>
2024-11-14 14:40:46 -0700

    Modified Order Of Regions To Simplify

--- js/improvements/csharp.js
+++ js/improvements/csharp.js
@@ -1,61 +1,5 @@
 // A JavaScript file thats make JavaScript work more like C#, one line at a time.
 
-// #region String Properties and Methods
-Object.defineProperty(String, "isNullOrWhiteSpace", {
-	/**
-	 * Indicates whether a specified string is null, empty, or consists only of white-space characters.
-	 * @param {string} string
-	 * @returns {boolean}
-	 */
-	value: function(string) {
-		const e = new Error();
-
-		if (typeof (string) != "string") {
-			e.name = "ArgumentInvalidTypeException";
-			e.message = `Function expected a string argument, got ${typeof (string)} instead.`;
-			throw e;
-		}
-
-		return !string || !string.trim();
-	},
-	writable: false
-});
-
-Object.defineProperty(String, "empty", {
-	value: "",
-	writable: false
-});
-
-Object.defineProperty(String.prototype, "contains", {
-	/**
-	 * Returns a value indicating whether a specified string occurs within this string, using the specified comparison rules.
-	 * @param {string} searchString
-	 * @param {boolean | undefined} ignoreCase
-	 * @returns {boolean}
-	 */
-	value: function(searchString, ignoreCase) {
-		const e = new Error();
-
-		if (typeof (searchString) != "string") {
-			e.name = "ArgumentInvalidTypeException";
-			e.message = `Function expected a string argument, got ${typeof (searchString)} instead.`;
-			throw e;
-		}
-
-		if (typeof ignoreCase != "boolean") {
-			ignoreCase = false;
-		}
-
-		if (ignoreCase) {
-			return this.toLowerCase().includes(searchString.toLowerCase());
-		} else {
-			return this.includes(searchString);
-		}
-	},
-	writable: false
-});
-// #endregion
-
 // #region Exception Classes
 /**
  * @public
@@ -137,4 +81,52 @@ class HTMLElementsNotFoundException extends Exception {
 		this.name = "HTMLElementsNotFoundException";
 	}
 }
+// #endregion
+
+// #region String Properties and Methods
+Object.defineProperty(String, "isNullOrWhiteSpace", {
+	/**
+	 * Indicates whether a specified string is null, empty, or consists only of white-space characters.
+	 * @param {string} string
+	 * @returns {boolean}
+	 */
+	value: function(string) {
+		if (typeof (string) != "string") {
+			throw new ArgumentInvalidTypeException(`Function expected a string argument, got ${typeof (string)} instead.`);
+		}
+
+		return !string || !string.trim();
+	},
+	writable: false
+});
+
+Object.defineProperty(String, "empty", {
+	value: "",
+	writable: false
+});
+
+Object.defineProperty(String.prototype, "contains", {
+	/**
+	 * Returns a value indicating whether a specified string occurs within this string, using the specified comparison rules.
+	 * @param {string} searchString
+	 * @param {boolean | undefined} ignoreCase
+	 * @returns {boolean}
+	 */
+	value: function(searchString, ignoreCase) {
+		if (typeof (searchString) != "string") {
+			throw new ArgumentInvalidTypeException(`Function expected a string argument, got ${typeof (searchString)} instead.`);
+		}
+
+		if (typeof ignoreCase != "boolean") {
+			ignoreCase = false;
+		}
+
+		if (ignoreCase) {
+			return this.toLowerCase().includes(searchString.toLowerCase());
+		} else {
+			return this.includes(searchString);
+		}
+	},
+	writable: false
+});
 // #endregion
