2f6536a3acbcd0afbf8f7e236e76c0054b2c0720
Computer Virus <edwardrainfall@gmail.com>
2024-12-13 16:04:48 -0700

    Implemented getting DOM Nodes in js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// #region Get DOM Nodes
+const header = get("header");
+const main = get("main");
+const sidebarLeft = get(".sidebar.sidebar-left");
+const sidebarRight = get(".sidebar.sidebar-right");
+const footer = get("footer");
+// #endregion
